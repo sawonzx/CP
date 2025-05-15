179ed6c3451169d7bb4b1839f572d547ffceb508
Mohammad Robiul Islam <150071152+sawonzx@users.noreply.github.com>
2025-05-15 17:21:52 +0600

    Update 7.cpp

--- CP-31 Sheet/800/7.cpp
+++ CP-31 Sheet/800/7.cpp
@@ -1,1 +1,41 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+bool string_check(int n, int m,string new_N, string M){
+  
+	for(int i = 0; i<=n - m; i++){
+		if(M == new_N.substr(i, m)) return true;
+	}
+	return false;
+}
+
+int main(){
+  
+	int t;
+	cin >> t;
+	while(t--){
+    
+		int n,m;
+		cin >> n >> m;
+
+		string N,M;
+		cin >> N >> M;
+
+		int action = -1;
+		int new_n = n;
+		string new_N = N;
+
+		for(int i = 0; i<=5; i++){			
+
+			if(string_check(new_n, m, new_N, M)){
+				action = i;
+				break;
+			}
+			new_N += new_N;
+			new_n = new_N.size();
+		}
+    
+		cout << action << endl;
+		}
+	}
